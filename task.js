5ae3cb360b8c81f597ba543ea1841c592d14c2c0
Long Ouyang <longouyang@gmail.com>
2013-04-12 14:54:10 -0700

    Add 12 word catch trial, fix a length recording bug, some reindenting

--- task.js
+++ task.js
@@ -25,7 +25,7 @@ function setupRecall(options) {
     function endRecall() {
       var recallText = $("#recall-text").val(),
           responseWords = recallText.split("\n").map(function(y) { return y.toLowerCase() }),
-          n = responseWords.length,
+          n = correctWords.length,
           score = 0;
 
       for(var i = 0; i < n; i++) {
@@ -71,18 +71,18 @@ var getOuterStream = function(params0) {
 
   var stream =
         $z.stream({
-	  trials: params0.trials,
-	  after: params0.after,
-	  trialStart: function(params) {
-	    var localMathErrors = 0;
-	    $("#math-error-warning").hide();
-	    
-	    var localTrials = _(params.words).map(function(word, i) {
-	      return $.extend( {word: word}, params.problems[i] );
-	    });
+	        trials: params0.trials,
+	        after: params0.after,
+	        trialStart: function(params) {
+	          var localMathErrors = 0;
+	          $("#math-error-warning").hide();
+	          
+	          var localTrials = _(params.words).map(function(word, i) {
+	            return $.extend( {word: word}, params.problems[i] );
+	          });
 
-	    var innerStream = $z.stream({
-	      trials: localTrials,
+	          var innerStream = $z.stream({
+	            trials: localTrials,
               after:  function() {
                 $("#math-error-warning").hide();
                 $("#math-percentage").hide();
@@ -112,6 +112,7 @@ var getOuterStream = function(params0) {
                     var v = recallProps;
                     
                     stream.end({
+                      length: v.length,
                       recallScore: v.score,
                       recallPerfect: v.perfect,
                       correctWords: v.correctWords,
@@ -128,56 +129,56 @@ var getOuterStream = function(params0) {
                 });
                 
               },
-	      trialStart: function(params1) {
-	        $z.showSlide("math");
-	        totalMathProblems++;
-	        
-	        var prompt = params1.prompt,
-		    promptCorrect = params1.promptCorrect,
+	            trialStart: function(params1) {
+	              $z.showSlide("math");
+	              totalMathProblems++;
+	              
+	              var prompt = params1.prompt,
+		                promptCorrect = params1.promptCorrect,
                     word = params1.word;
 
-	        if (localMathErrors >= 3) {
-		  $("#math-error-warning").show();
-	        }
+	              if (localMathErrors >= 3) {
+		              // $("#math-error-warning").show();
+	              }
 
                 $("#math-prompt").text(prompt);
                 $("#word-text").text(word);
 
                 var timeStart = (new Date()).getTime();
 
-	        // set up true and false button clicks
+	              // set up true and false button clicks
                 var keyHandler = function(e) {
                   var keyCode = e.which;
                   if (!(keyCode == 80 || keyCode == 81)) {
                     $(document).one("keydown", keyHandler);
                   } else {
                     var rt = (new Date()).getTime() - timeStart,
-		        response = (keyCode == 80 ? true : false),
-		        correct = (response == promptCorrect ? 1 : 0);
+		                    response = (keyCode == 80 ? true : false),
+		                    correct = (response == promptCorrect ? 1 : 0);
 
-		    if (!correct) {
-		      localMathErrors++;
-		      totalMathErrors++;
-		    }
+		                if (!correct) {
+		                  localMathErrors++;
+		                  totalMathErrors++;
+		                }
 
-		    $z.showSlide("word");
-		    wait(1000, function() {
+		                $z.showSlide("word");
+		                wait(1000, function() {
                       innerStream.end({
-		        response: response,
-		        correct: correct,
-		        rt: rt
-		      });
-		    });
+		                    response: response,
+		                    correct: correct,
+		                    rt: rt
+		                  });
+		                });
                   }
                 };
 
                 $(document).one("keydown", keyHandler);
                 
-	      }
-	    });
-	    
-	    innerStream.start();
-	  }
+	            }
+	          });
+	          
+	          innerStream.start();
+	        }
         });
 
   return stream;
@@ -208,9 +209,9 @@ var trials1 = [{words: ["disc","form"],
                              });
 
 var allWords = ["arch","horn","crab","vine","note",
-                 "fort","seal","joke","clay","camp",
-                 "palm","dart","whip","cube","hive",
-                 "cage","pest","tape","hail","tide"],
+                "fort","seal","joke","clay","camp",
+                "palm","dart","whip","cube","hive",
+                "cage","pest","tape","hail","tide"],
     allMath = [
       {prompt: "24 + 3 = 27", promptCorrect: true, gradeLevel: 31},
       {prompt: "18/3 = 7", promptCorrect: false, gradeLevel: 46},
@@ -242,17 +243,36 @@ var allWords = ["arch","horn","crab","vine","note",
       // bah, mutation
       realWords = realWords.slice(size);
       realMath = realMath.slice(size);
-          
+      
       return {
         size: size,
         words: words,
         problems: problems
       };
+    }).concat({ // add 12-word catch trial
+      size: 12,
+      words: ["book","tree","face","coat","song","wind",
+              "rain","shop","hall","door","seat","note"],
+      problems: [
+        {prompt: "11 - 2 = 9", promptCorrect: true},
+        {prompt: "3 x 8 = 25", promptCorrect: false},
+        {prompt: "75 - 22 = 63", promptCorrect: false},
+        {prompt: "14/2 = 7", promptCorrect: true},
+        {prompt: "31 + 19 = 50", promptCorrect: true},
+        {prompt: "3 x 4 = 22", promptCorrect: false},
+        {prompt: "34 + 12 = 46", promptCorrect: true},
+        {prompt: "17 + 48 = 55", promptCorrect: false},
+        {prompt: "56/8 = 8", promptCorrect: true},
+        {prompt: "4 x 2 = 9", promptCorrect: false},
+        {prompt: "24 - 8 = 16", promptCorrect: true},
+        {prompt: "19 + 12 = 41", promptCorrect: false}
+      ] 
     }),
+    
     stream3 = getOuterStream({
       trials: trials3,
       after: function() {
-        var completed = this.completed,
+        var completed = this.completed.filter(function(x) { return x.length < 12}),
             recallScores = completed.map(function(x) { return x.recallScore / x.correctWords.length }),
             meanRecallScore = recallScores.sum()/recallScores.length,
             mathErrors = completed.map(function(x) { return x.mathTotalErrors }).sum(),
@@ -260,12 +280,15 @@ var allWords = ["arch","horn","crab","vine","note",
             mathScore = (mathTotals - mathErrors) / mathTotals,
             mathRts = [].concat.apply([], completed.pluck("mathRts")),
             meanMathRt = mathRts.sum() / mathRts.length;
+
+        var catchTrial = this.completed[this.completed.length - 1];
         
         if (window.opener) {
           window.opener.done();
         }
         
         var payload = _.extend({
+          passedCatchTrial: !catchTrial.recallPerfect,
           meanRecallScore: meanRecallScore,
           mathScore: mathScore,
           meanMathRt: meanMathRt,
